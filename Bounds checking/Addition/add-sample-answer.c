5be10e2bc412ab2475a764dd1407c3e83bebedc7
CHW <141213003+Chw41@users.noreply.github.com>
2024-05-06 20:30:15 +0800

    Update add-sample-answer.c

--- Bounds checking/Addition/add-sample-answer.c
+++ Bounds checking/Addition/add-sample-answer.c
@@ -10,6 +10,7 @@ int main()
     while(S<N){
         int si1=0,si2=0,result=0;
         scanf("%d %d", &si1, &si2);
+        // 檢查加法運算:
         // 如果 si1 是正數且 si2 與 INT_MAX - si1 的總和超過了 INT_MAX，會導致overflow
         // 如果 si1 是負數且 si2 與 INT_MIN - si1 的總和低於 INT_MIN，也會導致overflow
         if ((si1 > 0 && si2 > INT_MAX - si1) || (si1 < 0 && si2 < INT_MIN - si1)){
