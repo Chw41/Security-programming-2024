a80b635f891cd8f5ec2ab89e109d3117320a4874
CHW <141213003+Chw41@users.noreply.github.com>
2024-05-13 13:34:45 +0800

    Update multiplication-sample-answer.c

--- Bounds checking/Multiplicaion/multiplication-sample-answer.c
+++ Bounds checking/Multiplicaion/multiplication-sample-answer.c
@@ -15,7 +15,8 @@ int main()
         // 2. si1 和 si2 都是負數，且 si1 乘以 si2 的結果會低於 INT_MIN，導致overflow
         // 3. si1 是正數且 si2 是負數，但 si2 乘以 si1 的結果會低於 INT_MIN，導致overflow
         // 4. si1 是負數且 si2 是正數，但 si1 乘以 si2 的結果會低於 INT_MIN，導致overflow
-        if ((si1 > 0 && si2 > 0 && si1 > INT_MAX / si2) || (si1 < 0 && si2 < 0 && si1 < INT_MIN / si2) || (si1 > 0 && si2 < 0 && si2 < INT_MIN / si1) || (si1 < 0 && si2 > 0 && si1 < INT_MIN / si2)){
+        if ((si1 > 0 && si2 > 0 && si1 > INT_MAX / si2) || (si1 < 0 && si2 < 0 && si1 < INT_MIN / si2) || 
+            (si1 > 0 && si2 < 0 && si2 < INT_MIN / si1) || (si1 < 0 && si2 > 0 && si1 < INT_MIN / si2)){
                     printf("Error detected!\n");
                 }
                 else{
