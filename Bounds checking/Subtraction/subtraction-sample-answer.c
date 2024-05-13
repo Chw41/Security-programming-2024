c63e79d05b37f6ae43b55817de1e03033dfea5e4
CHW <141213003+Chw41@users.noreply.github.com>
2024-05-13 13:37:57 +0800

    Update subtraction-sample-answer.c

--- Bounds checking/Subtraction/subtraction-sample-answer.c
+++ Bounds checking/Subtraction/subtraction-sample-answer.c
@@ -10,6 +10,7 @@ int main()
     while(S<N){
         int si1=0,si2=0,result=0;
         scanf("%d %d", &si1, &si2);
+        // 合法範圍: -2147483648 ~ 2147483647
         // 檢查減法運算
         // 如果 si2 是正數且 si1 與 INT_MIN + si2 的總和低於 INT_MIN，則減法將導致overflow
         // 如果 si2 是負數且 si1 與 INT_MAX + si2 的總和超過 INT_MAX，也導致溢出overflow
